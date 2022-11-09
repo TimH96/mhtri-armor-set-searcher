f3b0358ede691b0765ba02c9320f7b199bfd7f2f
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-09 11:49:45 +0100

    WIP

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,8 +1,17 @@
-import { getArms, getChest, getDecorations, getHead, getLegs, getSkillActivationMap, getSkillNameMap, getWaist } from '../../data-provider/data-provider.module'
+import { getArms, getChest, getDecorations, getHead, getLegs, getSkillActivationMap, getSkillCategories, getSkillNameMap, getWaist } from '../../data-provider/data-provider.module'
 import DataInput from '../../searcher/models/DataInput'
 import { search } from '../../searcher/searcher.module'
 
+const htmlToElement = (html: string): Node => {
+  const template = document.createElement('template')
+  html = html.trim()
+  template.innerHTML = html
+  return template.content.firstChild as Node
+}
+
 async function main () {
+  const skillCategories = await getSkillCategories()
+
   const data: DataInput = {
     head: await getHead(),
     chest: await getChest(),
@@ -22,6 +31,33 @@ async function main () {
       skillActivations: [],
     },
   )
+
+  //////////////////////////////////////////
+
+  for (const index in skillCategories) {
+    const category = skillCategories[index]
+    const node = htmlToElement(`
+      <div class="search-picker-category" id="search-picker-category-${index}" data-category="${index}">
+        ${category}
+      </div>
+    `)
+    console.log(category)
+    document.getElementById("search-skill-picker")!.appendChild(node)
+  }
+
+  data.skillActivation.forEach((activationList) => {
+    activationList
+      .filter(activation => activation.isPositive)
+      .reverse()
+      .forEach((activattion) => {
+        const node = htmlToElement(`
+          <div class="search-picker-activation" data-skill="${activattion.requiredSkill}" data-points="${activattion.requiredPoints}">
+            ${activattion.name}
+          </div>
+        `)
+        document.getElementById(`search-picker-category-${activattion.category}`)!.appendChild(node)
+      })
+  })
 }
 
 main()
