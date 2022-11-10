7597b47335c90f663d1b296e9f4b58a7424259dc
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-10 17:29:10 +0100

    implement deserialize

--- src/data-provider/models/equipment/UserCharmList.ts
+++ src/data-provider/models/equipment/UserCharmList.ts
@@ -1,5 +1,9 @@
 import { range } from '../../../helper/range.helper'
+import Skill from '../skills/Skill'
+import SkillNameMap from '../skills/SkillNameMap'
 import Charm from './Charm'
+import EquipmentCategory from './EquipmentCategory'
+import Slots from './Slots'
 
 export default class UserCharmList {
   // eslint-disable-next-line no-use-before-define
@@ -33,7 +37,7 @@ export default class UserCharmList {
   /** serializes charm list as csv */
   serialize () {
     return this.list.map((charm) => {
-      let s = []
+      const s = []
 
       const skillArray = Array.from(charm.skills.entries())
       for (const skill of skillArray) {
@@ -43,12 +47,58 @@ export default class UserCharmList {
       const amountOfSkills = skillArray.length
       // eslint-disable-next-line no-unused-vars
       for (const _ in range(amountOfSkills, 2)) {
-        s.push(`,,`)
+        s.push(',,')
       }
 
       s.push(`${charm.slots}`)
 
-      return s.join("")
-    }).join("\n")
+      return s.join('')
+    }).join('\n')
+  }
+
+  /** populate charm list from csv */
+  deserialize (csv: string, skillNames: SkillNameMap) {
+    const newList = []
+
+    for (const charm of csv.split('\n')) {
+      const spl = charm.split(',')
+
+      const slots = parseInt(spl[4])
+      const skills = [[0, 1], [2, 3]]
+        .filter(([_, j]) => !isNaN(parseInt(spl[j])))
+        .map(([i, j]) => {
+          const name = spl[i]
+
+          const id = Array.from(skillNames.entries())
+            .find(([_, n]) => {
+              return n === name
+            })![0]
+
+          // build skill model
+          const skill: Skill = {
+            name,
+            points: parseInt(spl[j]),
+            id,
+          }
+          return skill
+        })
+
+      const newCharm: Charm = {
+        name: 'charm',
+        category: EquipmentCategory.CHARM,
+        slots: slots as Slots,
+        skills: new Map(skills.map((skill) => {
+          return [skill.id, {
+            id: skill.id,
+            name: skillNames.get(skill.id)!,
+            points: skill.points,
+          }]
+        })),
+      }
+
+      newList.push(newCharm)
+    }
+
+    this.list = newList
   }
 }
