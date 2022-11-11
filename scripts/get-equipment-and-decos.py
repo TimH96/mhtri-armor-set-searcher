3f6e471e643ed6e4607860ee50d05449e1857288
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-11 13:16:44 +0100

    implement equipment script

---
+++ scripts/get-equipment-and-decos.py
@@ -0,0 +1,62 @@
+import json
+
+if __name__ == "__main__":
+    # read input
+    with open("./parsed-armor.json") as f:
+        armor = json.loads(f.read())
+    with open("./parsed-skills.json") as f:
+        decos = list(json.loads(f.read()))
+    with open("./parsed-decos.json") as f:
+        skills = list(json.loads(f.read()))
+    with open("./parsed-id-mapping.json") as f:
+        id_map = json.loads(f.read())
+
+    # init output
+    pieces_per_category = []
+
+    # iterate over armor
+    for (armor_category, (_, c)) in enumerate(armor.items()):
+        pieces_of_cat = []
+        for piece in c["pieces"]:
+            defense = {
+                "base": piece["defense"],
+                "max": piece.get("maxdefense", piece["defense"]),
+            }
+            lr_def = piece.get("maxlrdefense", None)
+            if lr_def:
+                defense["maxLr"] = lr_def
+            res = [
+                piece["fireres"],
+                piece["waterres"],
+                piece["iceres"],
+                piece["thunderres"],
+                piece["dragonres"],
+            ]
+            b = piece["blade"]
+            g = piece["gunner"]
+            t: int
+            if b and g:
+                t = 0
+            elif b:
+                t = 1
+            else:
+                t = 2
+            new_s = {id_map[k]: v for (k, v) in piece["skills"].items()}
+
+            modeled_piece = {
+                "name": piece["name"],
+                "slots": piece["slots"],
+                "category": armor_category + 1,
+                "rarity": piece["rarity"],
+                "type": t,
+                "defense": defense,
+                "resistance": res,
+                "skills": new_s,
+            }
+            pieces_of_cat.append(modeled_piece)
+        pieces_per_category.append(pieces_of_cat)
+
+    # save files
+    for (i, cat_name) in enumerate(["head", "chest", "arms", "waist", "legs"]):
+        with open(f"../data/{cat_name}.json", "w") as f:
+            f.write(json.dumps(pieces_per_category[i], indent=4, sort_keys=True))
