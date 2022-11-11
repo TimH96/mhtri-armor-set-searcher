0d386b3be6c1f58a3c38c6d25d312f8dc47bd7fd
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-11 12:28:18 +0100

    implement id mapping

--- scripts/get-skills.py
+++ scripts/get-skills.py
@@ -10,10 +10,11 @@ if __name__ == "__main__":
     # init output
     skill_names = {}
     skill_activations = {}
+    id_mapping = {}
 
     # iterate over skills
     name_list = [x["name"] for x in skill_map.values()]
-    for skill in skills[: len(skills) - 1]:  # remove Torso Up
+    for (i, skill) in enumerate(skills[: len(skills) - 1]):  # remove Torso Up
         # get attributes
         name = skill["name"]
         mapped_i = str(name_list.index(name))
@@ -37,9 +38,12 @@ if __name__ == "__main__":
         # populate output
         skill_names[id] = name
         skill_activations[id] = activations
+        id_mapping[i] = id
 
     # save files
     with open("../data/skill-names.json", "w") as f:
         f.write(json.dumps(skill_names, indent=4, sort_keys=True))
     with open("../data/skills.json", "w") as f:
         f.write(json.dumps(skill_activations, indent=4, sort_keys=True))
+    with open("./parsed-id-mapping.json", "w") as f:
+        f.write(json.dumps(id_mapping, indent=4, sort_keys=True))
