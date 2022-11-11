7f1d0c8a2f11eee7b1cef1f15adc960bc8289d56
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-11 15:34:43 +0100

    make charms also skilleditem

--- src/data-provider/models/equipment/Charm.ts
+++ src/data-provider/models/equipment/Charm.ts
@@ -1,6 +1,5 @@
 import Equipment from './Equipment'
-import EquipmentSkills from './EquipmentSkills'
+import SkilledItem from './SkilledItem'
 
-export default interface Charm extends Equipment {
-    skills: EquipmentSkills
+export default interface Charm extends Equipment, SkilledItem {
 }
