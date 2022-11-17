19a13e62f52ece3be4553f8851ab9623bdad38aa
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-17 19:44:14 +0100

    implement more skills

--- src/app/ui/search-controls.component.ts
+++ src/app/ui/search-controls.component.ts
@@ -6,15 +6,15 @@ import { search } from '../../searcher/searcher.module'
 import { getGlobalSettings } from './global-settings.component'
 import { getSkillActivations, resetSkillActivations } from './picker.component'
 import { renderResults } from './search-results.component'
+import SkillActivation from '../../data-provider/models/skills/SkillActivation'
 
-const searchLogic = (equData: StaticEquipmentData, skillData: StaticSkillData) => {
+const arrangeSearchData = (skillData: StaticSkillData) => {
   // build params
   const globalSettings = getGlobalSettings()
   const skillActivations = getSkillActivations(skillData)
 
   // return if no skill selected
   if (skillActivations.length === 0) {
-    alert('Please select at least one skill')
     return
   }
 
@@ -39,6 +39,17 @@ const searchLogic = (equData: StaticEquipmentData, skillData: StaticSkillData) =
     skillActivations: sanitizedSkillActivations,
   }
 
+  return searchParams
+}
+
+const searchLogic = (equData: StaticEquipmentData, skillData: StaticSkillData) => {
+  const searchParams = arrangeSearchData(skillData)
+
+  if (!searchParams) {
+    alert('Please select at least one skill')
+    return
+  }
+
   // search for sets
   const result = search(
     // equData.armor.map(x => x.filter(y => y.name.startsWith("Helios") && y.name.endsWith("+"))),
@@ -53,6 +64,48 @@ const searchLogic = (equData: StaticEquipmentData, skillData: StaticSkillData) =
   renderResults(result, skillData, searchParams)
 }
 
+const moreSkillsLogic = (equData: StaticEquipmentData, skillData: StaticSkillData) => {
+  const searchParams = arrangeSearchData(skillData)
+
+  if (!searchParams) {
+    alert('Please select at least one skill')
+    return
+  }
+
+  const charms = UserCharmList.Instance.get()
+
+  const aquirableSkills: SkillActivation[] = []
+
+  for (const actMap of skillData.skillActivation) {
+    const sActs = actMap[1]
+
+    const processedActs = sActs
+      .filter(act => act.requiredPoints >= 0)
+      .filter(act => !searchParams.skillActivations.map(x => x.id).includes(act.id))
+      .filter(act => !searchParams.skillActivations.find(x => act.requiredSkill === x.requiredSkill && act.requiredPoints < x.requiredPoints))
+      .sort((a, b) => b.requiredPoints - a.requiredPoints)
+
+    for (const act of processedActs) {
+      const newParams: SearchConstraints = {
+        ...searchParams,
+        limit: 1,
+        skillActivations: searchParams.skillActivations.concat(act),
+      }
+
+      const r = search(
+        equData.armor,
+        equData.decorations,
+        charms,
+        newParams,
+        skillData,
+      )
+
+      if (!r) break
+      else aquirableSkills.push(act)
+    }
+  }
+}
+
 const resetLogic = () => {
   resetSkillActivations()
 }
@@ -60,10 +113,14 @@ const resetLogic = () => {
 /** attach handlers for control buttons */
 export const attachControlListeners = (equData: StaticEquipmentData, skillData: StaticSkillData) => {
   const searchBtn = document.getElementById('search-btn') as HTMLButtonElement
+  const moreSkillsBtn = document.getElementById('more-btn') as HTMLButtonElement
   const resetBtn = document.getElementById('reset-btn') as HTMLButtonElement
 
   searchBtn.addEventListener('click', () => {
     searchLogic(equData, skillData)
+  })
+  moreSkillsBtn.addEventListener('click', () => {
+    moreSkillsLogic(equData, skillData)
   })
   resetBtn.addEventListener('click', () => {
     resetLogic()
