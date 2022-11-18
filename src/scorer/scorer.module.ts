c8453ad7668cba1643e5f843f5324eb1590dfafc
TimHasert <27998143+TimH96@users.noreply.github.com>
2022-11-18 11:54:38 +0100

    default score in case of wanted skills cancelling each other out

--- src/scorer/scorer.module.ts
+++ src/scorer/scorer.module.ts
@@ -16,7 +16,12 @@ const getScoreFromSkillMap = (m: EquipmentSkills, w: EquipmentSkills): number =>
 const evaluateListOfDecos = (decos: Decoration[], wantedSkills: EquipmentSkills): DecoPermutation => {
   const skillMap: EquipmentSkills = new EquipmentSkills()
   decos.forEach(deco => skillMap.addSkills(deco.skills))
-  const score = getScoreFromSkillMap(skillMap, wantedSkills)
+
+  // get max of default and computed score
+  // default score can only be higher than computed when the decos of 2 wanted skills cancel each other out (e.g. handicraft and sharpness)
+  const computedScore = getScoreFromSkillMap(skillMap, wantedSkills)
+  const defaultScore = Math.max(...Array.from(skillMap.values()))
+  const score = (Math.max(computedScore, defaultScore))
 
   return {
     skills: skillMap,
